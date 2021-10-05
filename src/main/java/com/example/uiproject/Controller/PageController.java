cf5be19083cb823bd70e6290efc6564f303b8e61
Silke Marie <silk2918@stud.kea.dk>
2021-10-05 13:13:40 +0200

    changed link looks to not have underline

--- src/main/java/com/example/uiproject/Controller/PageController.java
+++ src/main/java/com/example/uiproject/Controller/PageController.java
@@ -10,6 +10,30 @@ public class PageController {
     @GetMapping("/")
     public String getIndex (){
         return "index";
+    }
 
+    @GetMapping("/0-5")
+    public String getPreschool (){
+        return "preschool";
+    }
+
+    @GetMapping("/6-10")
+    public String getMiddleschool (){
+        return "middleschool";
+    }
+
+    @GetMapping("/gym")
+    public String getHighschool (){
+        return "highschool";
+    }
+
+    @GetMapping("/tips")
+    public String getTips (){
+        return "tips";
+    }
+
+    @GetMapping("/about")
+    public String getAbout (){
+        return "about";
     }
 }
